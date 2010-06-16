236b8e0cd8720f1eb8cfc92b90ee5db1b8f0d71e
thoka <thomas.kalka@googlemail.com>
2010-06-16 17:48:08 +0200

    cleanup; rebuild now calls also j2py/transform_all.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# rebuild everything. you have to provide a suitable configuration in ./configure ...
 
 ./bootstrap
 ./configure --prefix=/usr --with-aterm=/usr --with-sdf=/usr --with-strategoxt=/usr --enable-bootstrap
 make clean
 make
+j2py/transform_all.sh
 
