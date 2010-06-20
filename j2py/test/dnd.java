1f04e0c6ab84484ea81cb6842b8e23ca8235e3e1
thoka <thomas.kalka@googlemail.com>
2010-06-20 22:41:40 +0200

    remove " from comments, pp-aterm has a bug and dont like it

--- j2py/test/dnd.java
+++ j2py/test/dnd.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2009 Fred Sauer
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * Licensed under the Apache License, Version 2.0 (the ''License''); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -39,6 +39,8 @@ import java.util.HashMap;
  * 
  * @see #registerDropController(DropController)
  */
+
+
 public class PickupDragController extends AbstractDragController {
   /**
    * Private implementation class to store widget information while dragging.
