797ca064c16552bfa2cabe52448cea0c4ce10b50
thoka <thomas.kalka@googlemail.com>
2010-06-25 11:50:55 +0200

    fix test

--- j2py/test/java2python/QuickArray.java
+++ j2py/test/java2python/QuickArray.java
@@ -1,8 +1,8 @@
 public class QuickArray  {
     public static void main(String[] args) {
 	Integer b[] = {4, 5, 6};
-        System.out.println(b[0].toString());
-        System.out.println(b[1].toString());
-        System.out.println(b[2].toString());
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
     }
 }
