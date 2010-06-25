53cacf7bed5f6451c12bc0a4cceae50f9ebe0f08
thoka <thomas.kalka@googlemail.com>
2010-06-25 13:27:23 +0200

    do not fetch git in install...

--- install_on_debian_lenny.sh
+++ install_on_debian_lenny.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install dependencies on debian lenny, 
+# then build
+
 aptitude install openssh-server sudo
 aptitude install git-core bzip2 autoconf libtool pkg-config make python python-yaml
 
@@ -15,8 +18,6 @@ dpkg -i aterm_2.5-1_i386.deb
 dpkg -i sdf2-bundle_2.4-1_i386.deb
 dpkg -i strategoxt_0.17-1_i386.deb
 
-cd
-git clone http://github.com/thoka/java2python.git jf2py
-cd jf2py
+cd ..
 ./rebuild.sh
 
