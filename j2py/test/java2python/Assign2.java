83a9f331f856c4608b9b29753203026074f1463f
thoka <thomas.kalka@googlemail.com>
2010-06-26 18:39:58 +0200

    + >>>, ++i, i++

--- j2py/test/java2python/Assign2.java
+++ j2py/test/java2python/Assign2.java
@@ -1,9 +1,40 @@
 class Assign2 {
     public static void main(String[] args) {
-        int x = 42;
+        int x = 42, y=7;
         System.out.println(x);
 
-        x >>>= 1;
+        x >>>= 1;        
         System.out.println(x);
+        
+        x = 2 + x >>> 1;
+        System.out.println(x);
+        System.out.println(y);
+
+        y = x++;
+        System.out.println(x);
+        System.out.println(y);
+                
+        y = ++x;
+        System.out.println(x);
+        System.out.println(y);
+                
+
+        y = x--;
+        System.out.println(x);
+        System.out.println(y);
+                
+        y = --x;
+        System.out.println(x);
+        System.out.println(y);
+
+        x++; --y;
+        System.out.println(x);
+        System.out.println(y);
+                
+        --y; ++x;
+        System.out.println(x);
+        System.out.println(y);
+
+
     }
 }
