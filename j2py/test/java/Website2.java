2611e8ecf8a3c95ab66ce4f7fe9d34ab61cf2f6d
thoka <thomas.kalka@googlemail.com>
2010-07-04 11:25:38 +0200

    fix some test src

--- j2py/test/java/Website2.java
+++ j2py/test/java/Website2.java
@@ -1,6 +1,5 @@
 //Brewing Java: A Tutorial Copyright 1995-1998, 2000-2002, 2004-2006 Elliotte Rusty Harold
-
-public class Website {
+class Website {
 
   public String name;
   public String url;
@@ -15,14 +14,18 @@ public class Website {
      System.out.println(name + " at " + url + " is " + description);
      System.out.println("Number of class instances: " + site_count);
   }
+}
+
+
+public class Website2 {
 
   public static void main(String args[]) {
-Website w = new Website();
-w.name = "Jorika";
+    Website w = new Website();
+    w.name = "Jorika";
     w.url = "http://jorika.edu";
     w.description = "Really cool!";
     w.printme();
-    
+    //
     Website w2 = new Website();
     w.name = "Foggi 4";
     w.url = "http://foggi4youevery.fa";
